cc17b2ae32f971d7ca2044aa78c5e99f57f54eb1
Rajesh97266 <rajesh97266@gmail.com>
2024-11-04 15:11:04 +0530

    filter

--- script.js
+++ script.js
@@ -80,6 +80,26 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const createUserNames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.userName = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUserNames(accounts);
+console.log(accounts);
+
+// const userName = user
+//   .toLocaleLowerCase()
+//   .split(' ')
+//   .map(name => name[0])
+//   .join('');
+// console.log(userName);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -176,27 +196,38 @@ displayMovements(account1.movements);
 // };
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// const euroToUsd = 1.1;
+const euroToUsd = 1.1;
 
-// const movementsUSD = movements.map(mov => mov * euroToUsd);
+const movementsUSD = movements.map(mov => mov * euroToUsd);
 
-// console.log(movementsUSD);
+console.log(movementsUSD);
 
-// const movementsUSDFor = [];
-// for (const mov of movements) {
-//   movementsUSDFor.push(mov * euroToUsd);
-// }
+const movementsUSDFor = [];
+for (const mov of movements) {
+  movementsUSDFor.push(mov * euroToUsd);
+}
 
-// console.log(movementsUSDFor);
+console.log(movementsUSDFor);
 
-// const movementsDescription = movements.map((movement, i) => {
-//   if (movement > 0) {
-//     return `Movement ${i + 1}: You deposited ${movement}`;
-//   } else {
-//     return `Movement ${i + 1}: You withdrew ${Math.abs(movement)}`;
-//   }
-// });
+const movementsDescription = movements.map((movement, i) => {
+  if (movement > 0) {
+    return `Movement ${i + 1}: You deposited ${movement}`;
+  } else {
+    return `Movement ${i + 1}: You withdrew ${Math.abs(movement)}`;
+  }
+});
 
-// console.log(movementsDescription);
+console.log(movementsDescription);
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(movements);
+console.log(deposits);
+
+const withdrawls = movements.filter(mov => mov < 0);
+
+console.log(withdrawls);
