3b43116158c592dad5f324c8cca2248239bced37
Rajesh97266 <rajesh97266@gmail.com>
2024-11-04 10:44:59 +0530

    third

--- script.js
+++ script.js
@@ -74,3 +74,36 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -1));
+console.log(arr.slice());
+console.log([...arr]);
+
+// console.log(arr.splice(2));
+arr.splice(-1);
+arr.splice(1, 2);
+console.log(arr);
+
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'f', 'i', 'h', 'g'];
+console.log(arr2.reverse());
+
+//concat
+const letters = arr.concat(arr2);
+console.log(letters);
+
+//join
+
+console.log(letters.join('-'));
+
+const arr3 = [23, 44, 88];
+console.log(arr3.at(1));
+
+console.log(arr3.at(-1));
+
+console.log('jonas'.at(-1));
